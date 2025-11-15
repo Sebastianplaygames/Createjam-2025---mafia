956813ceb4a29bc0a12c382141fc173334390c9c
Kabuum <thomsenribermartin@gmail.com>
2025-11-15 21:00:58 +0100

    melee stuff

---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,28 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public int damage = 1;
+    public float lifetime = 0.1f; // short-lived melee hitbox
+    bool hasHit = false;
+
+    
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasHit) return;
+        IDamagable target = other.GetComponent<IDamagable>();
+        if (target != null)
+        {
+            target.TakeDamage(damage);
+            hasHit = true;
+        }
+    }
+
+}
