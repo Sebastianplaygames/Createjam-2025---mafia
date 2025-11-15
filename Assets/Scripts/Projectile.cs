91dc9f367b15f4c69a0ce1da0125906887135b37
Kabuum <thomsenribermartin@gmail.com>
2025-11-15 12:55:33 +0100

    Some projectiles and layer additions

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,9 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
 
     public class Projectile : MonoBehaviour
     {
         public int damage = 1;
         public float speed = 10f;
         public float lifeTime = 3f;
-        
+        public Vector2 direction;
+
+        private void Start()
+        {
+            Destroy(gameObject, lifeTime);
+        }
+
+        private void Update()
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            IDamagable dmg = other.GetComponent<IDamagable>();
+            if (dmg != null)
+            {
+                dmg.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
     }
