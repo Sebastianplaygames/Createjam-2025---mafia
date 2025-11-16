bb342c57c55c42b11eddb1c4593fb6c2b06d0ce4
Sebastianplaygames <94458257+Sebastianplaygames@users.noreply.github.com>
2025-11-16 04:13:01 +0100

    added intro

--- Assets/Scripts/StartMissionButton.cs
+++ Assets/Scripts/StartMissionButton.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour
+public class StartMissionButton : MonoBehaviour
 {
 
 public void start(){
   //  print("test start");
 }
 
- public void StartGame() {
-    SceneManager.LoadScene("Intro");
+ public void StartMission() {
+    SceneManager.LoadScene("SampleScene");
  //   Debug.Log("Button clicked");
 }
 }
